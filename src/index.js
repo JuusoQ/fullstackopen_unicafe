40bc20a31ba08be64d4044edba9e1751277d8c4a
Juuso Haapanen <juuso@haapanen.biz>
2018-05-02 23:29:21 +0300

    Tehtävä 1.6 unicafe osa 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            good: 0,
+            neutral: 0,
+            bad: 0,
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Anna palautetta</h1>
+                <button onClick={()=>this.setState({good: this.state.good + 1})} >Hyvä</button>
+                <button onClick={()=>this.setState({neutral: this.state.neutral + 1})}>Neutraali</button>
+                <button onClick={()=>this.setState({bad: this.state.bad + 1})}>Huono</button>
+
+                <h1>Statistiikkaa</h1>
+                Hyvä: {this.state.good} <br/>
+                Neutraali: {this.state.neutral} <br/>
+                Huono: {this.state.bad}
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
